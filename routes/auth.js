714a60d0544f93019720d88a7db0e50e5a292a6b
Jill Kidman <jillkidman@gmail.com>
2022-07-20 15:01:07 -0700

    end of day 2

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,19 @@ const bcrypt = require('bcryptjs');
 const { uuid } = require('uuidv4');
 const { blogsDB } = require("../mongo");
 const { MongoMissingCredentialsError } = require("mongodb");
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+dotenv.config();
+
 
 const createUser = async (username, passwordHash) => {
+    const collection = await blogsDB().collection("users");
+    const user = {
+        username: username,
+        password: passwordHash,
+        uid: uuid()
+    };
     try {
-        const collection = await blogsDB().collection("users")
-        const user = {
-            username: username,
-            password: passwordHash,
-            uid: uuid() // uid stands for User ID. This will be a unique string that we will can to identify our user
-        }
         await collection.insertOne(user);
         return true;
     } catch (error) {
@@ -26,7 +30,6 @@ router.post("/register-user", async function (req, res, next) {
         const username = req.body.username
         const password = req.body.password
         const saltRounds = 5; // In a real application, this number would be somewhere between 5 and 10
-        let userSaveSuccess = false;
         const salt = await bcrypt.genSalt(saltRounds)
         const hash = await bcrypt.hash(password, salt)
         const userSaveSuccess = await createUser(username, hash);
@@ -38,18 +41,52 @@ router.post("/register-user", async function (req, res, next) {
 })
 
 router.post("/login-user", async function (req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const collection = await blogsDB().collection("users")
+
     try {
-        const collection = await blogsDB().collection("users")
         const user = await collection.findOne({
-            username: req.body.username
+            username: username
         })
-        const match = await bcrypt.compare(req.body.password, user.password);
-        res.status(200).json({ success: match })
+        const match = await bcrypt.compare(password, user.password);
+        if (match) {
+            const jwtSecretKey = process.env.JWT_SECRET_KEY;
+            const data = {
+                time: new Date(),
+                userId: user.uid
+            }
+            const token = jwt.sign(data, jwtSecretKey);
+            res.status(200).json({ success: true, token })
+            return;
+        }
+        res.json({ success: false });
     }
     catch (error) {
         res.status(500).json({ message: "Error logging in.", success: false })
     }
 })
+
+router.get("/validate-token", function (req, res) {
+    const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    try {
+        const token = req.header(tokenHeaderKey);
+        const verified = jwt.verify(token, jwtSecretKey);
+        if (verified) {
+            return res.json({ success: true });
+        } else {
+            // Access Denied
+            throw Error("Access Denied")
+        }
+    } catch (error) {
+        // Access Denied
+        return res.status(401).json({ success: true, message: String(error) });
+    }
+})
+
+
 
 
 module.exports = router
